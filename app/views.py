1148f00c6538212aa6f97b6a0b8bdcb46f05df83
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2024-03-31 20:19:56 +0900

    ルーティング調整

--- app/views.py
+++ app/views.py
@@ -4,10 +4,14 @@ from .models import Board
 from .forms import BoardForm
 
 def index(request):
-    boards = Board.objects.all()
+    boards = Board.objects.all().order_by('-updated_at')
     return render(request, 'index.html', {'boards': boards})
 
 def new(request):
+    form = BoardForm()
+    return render(request, 'new.html', {'form': form})
+
+def create(request):
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -17,14 +21,16 @@ def new(request):
         form = BoardForm()
     return render(request, 'new.html', {'form': form})
 
-def create(request):
-    pass
-
 def show(request, pk):
     board = Board.objects.get(pk=pk)
     return render(request, 'show.html', {'board': board})
 
 def edit(request, pk):
+    board = Board.objects.get(pk=pk)
+    form = BoardForm(instance=board)
+    return render(request, 'edit.html', {'form': form, 'board': board})
+
+def update(request, pk):
     board = Board.objects.get(pk=pk)
     if request.method == 'POST':
         form = BoardForm(request.POST, instance=board)
@@ -34,9 +40,6 @@ def edit(request, pk):
     else:
         form = BoardForm(instance=board)
     return render(request, 'edit.html', {'form': form, 'board': board})
-
-def update(request, pk):
-    pass
 
 def delete(request, pk):
     board = get_object_or_404(Board, pk=pk)
