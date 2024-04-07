468e201f51e70eaae3251753d20080e1025aaa56
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2024-04-07 16:15:59 +0900

    リレーション名の追加

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Board(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
