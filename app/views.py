5860711a87f4bfd75b253fdbe14c002a8b99970c
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2024-05-07 16:04:20 +0900

    BCCに修正

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from functools import wraps
 from django.http import JsonResponse
 from django.db.models import Count
 from django.db import models
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 from django.template.loader import render_to_string
 
@@ -110,7 +110,8 @@ def comment_create(request, pk):
             'user': request.user.username,
             'comment': comment.content,
         })
-        send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
+        email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [], bcc=recipient_list)
+        email.send()
 
     return redirect('show', pk=pk)
 
