1b7af45ff9416c77300dbb95418bb5460c8bb1ae
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2024-05-18 17:20:29 +0900

    インデントを修正

--- static/js/common.js
+++ static/js/common.js
@@ -4,24 +4,24 @@ $(document).ready(function(){
 });
 
 // お気に入りボタンの非同期通信
-    $(document).on('click', '.favorite-form', function(e) {
-        e.preventDefault();
-        let $form = $(this);
-        let url = $form.attr('action');
-        let data = $form.serialize();
+$(document).on('click', '.favorite-form', function(e) {
+    e.preventDefault();
+    let $form = $(this);
+    let url = $form.attr('action');
+    let data = $form.serialize();
 
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            dataType: 'json',
-        }).done(function(response){
-            if (response.status === 'success') {
-                $form.closest('td').html(response.html);
-            } else {
-                alert('There was an error: ' + response.message);
-            }
-        }).fail(function(response){
-            alert('There was an error processing your request. Please try again.');
-        });
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+    }).done(function(response){
+        if (response.status === 'success') {
+            $form.closest('td').html(response.html);
+        } else {
+            alert('There was an error: ' + response.message);
+        }
+    }).fail(function(response){
+        alert('There was an error processing your request. Please try again.');
     });
+});
